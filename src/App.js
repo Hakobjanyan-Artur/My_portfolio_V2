f45ea0c401b2db82259d853dd75d9aa662530c59
arturwebdev <hakobjanyan61@gmail.co>
2023-02-28 00:12:31 +0400

    navbar-UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-
+import './App.css'
+import { FaHouseUser } from 'react-icons/fa';
+import { FcAbout, FcServices, FcContacts } from 'react-icons/fc';
+import logo from './images/logo.png'
+import cv from './Resume Artur Hakobjanyan.pdf'
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <nav>
+        <div className='logo'>
+          <img src={logo} alt="" />
+        </div>
+        <ul>
+          <li><FaHouseUser className='nav-main' /> Main</li>
+          <li><FcAbout className='nav-icon' />About</li>
+          <li><FcServices />Services</li>
+          <li><FcContacts />Contact</li>
+        </ul>
+        <a href={cv} download className='nav-cv'>DOWNLOAD CV</a>
+      </nav>
+      <div className='main'>
+      </div>
     </div>
   );
 }
