e2e0e0d34710238507031810574090d4e55174d4
arturwebdev <hakobjanyan61@gmail.co>
2023-03-01 19:29:16 +0400

    main-pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import './App.css'
 import { FaHouseUser } from 'react-icons/fa';
-import { FcAbout, FcServices, FcContacts } from 'react-icons/fc';
+import { GiSkills } from 'react-icons/gi';
+import { FcAbout, FcContacts } from 'react-icons/fc';
 import logo from './images/logo.png'
 import cv from './Resume Artur Hakobjanyan.pdf'
+import { useEffect, useRef, useState } from 'react';
+import Main from './components/main/Main';
+import About from './components/about/Aout';
+import Skills from './components/skills/Skills';
+import Contact from './components/contact/Contact';
 
 function App() {
+  const mainRef = useRef(null)
+  const aboutRef = useRef(null)
+  const skillsRef = useRef(null)
+  const contactRef = useRef(null)
+  const [top, setTop] = useState(0)
+
+
+  const scrollHandler = (e) => {
+    // console.log('ecran ' + e.target.documentElement.scrollHeight);
+    // console.log('verevic minchev bar ' + e.target.documentElement.scrollTop);
+    // console.log('bar ' + window.innerHeight);
+    setTop(e.target.documentElement.scrollTop)
+
+    // console.log(e.target.documentElement.scrollTop)
+
+    // if (e.target.documentElement.scrollTop > 600) {
+    //   backRef.current.classList.add('width')
+    // } else {
+    //   backRef.current.classList.remove('width')
+    // }
+  }
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler)
+    return () => {
+      document.removeEventListener('scroll', scrollHandler)
+    }
+  }, [])
+
+
   return (
     <div className="App">
       <nav>
@@ -12,15 +48,17 @@ function App() {
           <img src={logo} alt="" />
         </div>
         <ul>
-          <li><FaHouseUser className='nav-main' /> Main</li>
-          <li><FcAbout className='nav-icon' />About</li>
-          <li><FcServices />Services</li>
-          <li><FcContacts />Contact</li>
+          <li onClick={(e) => { e.preventDefault(); mainRef.current.scrollIntoView({ behavior: "smooth" }) }}><FaHouseUser className='nav-main' /> Main</li>
+          <li onClick={(e) => { e.preventDefault(); aboutRef.current.scrollIntoView({ behavior: "smooth" }) }}><FcAbout />About</li>
+          <li onClick={(e) => { e.preventDefault(); skillsRef.current.scrollIntoView({ behavior: "smooth" }) }}><GiSkills className='nav-skills' />Skills</li>
+          <li onClick={(e) => { e.preventDefault(); contactRef.current.scrollIntoView({ behavior: "smooth" }) }}><FcContacts />Contact</li>
         </ul>
         <a href={cv} download className='nav-cv'>DOWNLOAD CV</a>
       </nav>
-      <div className='main'>
-      </div>
+      <div ref={mainRef} ><Main /></div>
+      <div ref={aboutRef}><About top={top} /></div>
+      <div ref={skillsRef}><Skills /></div>
+      <div ref={contactRef}><Contact /></div>
     </div>
   );
 }
---
+++ src/components/main/Main.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react'
+import mainPhoto from '../../images/main.jpg'
+
+export default function Main() {
+    const [aboutMe, setAboutMe] = useState('')
+
+    const text = [
+        'Hello, my name is Artur',
+        'I am front-end developer from Armenia',
+        'I love the IT world',
+        'I am passionate about creating great software that improves the lives of the people around me',
+        'Open to new opportunities in front-end development'
+    ]
+
+    let count = 0
+    let line = 0
+    let mainAbout = ''
+
+    const txtChange = (txt) => {
+        mainAbout += txt[line][count]
+        setAboutMe(mainAbout)
+        count++
+        if (count < txt[line].length) {
+            time()
+        }
+        if (count === txt[line].length) {
+            setTimeout(() => {
+                revers()
+            }, 1000)
+        }
+    }
+    const revers = () => {
+        mainAbout = mainAbout.slice(0, mainAbout.length - 1)
+        setAboutMe(mainAbout)
+
+        if (mainAbout.length > 0) {
+            timeRevers()
+        }
+        if (mainAbout.length === 0 && line === text.length - 1) {
+            line = 0
+            count = 0
+            txtChange(text)
+        }
+        if (mainAbout.length === 0) {
+            line++
+            count = 0
+            txtChange(text)
+        }
+    }
+
+    const timeRevers = () => {
+        setTimeout(() => {
+            revers()
+        }, Math.floor(Math.random() * 500 + 50))
+    }
+
+    const time = () => {
+        setTimeout(() => {
+            txtChange(text)
+        }, Math.floor(Math.random() * 500 + 50))
+    }
+
+    useEffect(() => {
+        txtChange(text)
+    }, [])
+
+
+    return (
+        <div className='main'>
+            <div className="main-content">
+                <div className="main-image-background">
+                    <div className="main-image">
+                        <img src={mainPhoto} alt="" />
+                    </div>
+                </div>
+                <h1 className="main-name">Artur Hakobjanyan</h1>
+                {/* <h2 className='profession'>Front-End Developer</h2> */}
+                <p className="main-about">{aboutMe + '|'}</p>
+            </div>
+        </div>
+    )
+}
